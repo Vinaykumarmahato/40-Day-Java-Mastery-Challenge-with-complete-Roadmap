811f60a4360698880f13b06a7a4913b6ef445853
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2023-11-14 09:52:17 -0800

    Create solution1duplicates.java

---
+++ Day1/solution1duplicates.java
@@ -0,0 +1,37 @@
+package Day1;
+
+public class Solution1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=29;
+		String name="Vinay Kumar java developer";
+		
+		System.out.println(name);
+		System.out.println(a);
+				
+
+	}
+
+}
+//📅 Day 1: Variables, Data Types, and Operators
+//This is the first day of our Java programming journey. Today, we will focus on the fundamental building blocks of Java: variables, data types, and operators.
+
+//📅 Variables
+//In Java, variables are used to store data. We will learn how to declare and initialize variables, including different data types like int, double, char, and more.
+
+//📅 Data Types
+//Understanding data types is crucial. We'll explore primitive data types (int, double, boolean, etc.) and reference data types (String), and discuss their use in Java programs.
+
+//📅 Operators
+//Operators are used for performing operations on variables and values. We'll cover arithmetic operators (+, -, *, /), relational operators (>, <, ==), and logical operators (&&, ||), among others.
+
+//📅 Type Casting
+//Sometimes, you'll need to convert data from one type to another. We'll explain how type casting works in Java.
+
+//📅 Expressions and Statements
+//In Java, expressions and statements play a vital role in building your programs. We'll explore what they are and how to use them effectively.
+
+//📅 Practice and Hands-On
+//To solidify our understanding, we will work on practical exercises and coding examples to apply what we've learned today.

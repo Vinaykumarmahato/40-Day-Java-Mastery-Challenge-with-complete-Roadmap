b88125907fb0d4567e1852827fd9fe482127a316
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2023-11-19 22:52:52 +0530

    Create basics.java

---
+++ Day5/basics.java
@@ -0,0 +1,29 @@
+package Day5;
+// public class hey{  Compile time error
+	
+// }
+public class Exception1 {
+	public static void main(String[] args) {
+	//	system.out.println(); // Syntax error
+		
+		
+		// Logical Error
+		int a=10;
+		int b=34;
+		int c=a-b;
+		
+		
+		// Run time error
+	//	int a=10;
+	//	int b=0;
+		
+		int 
+		result=a/b;
+		System.out.println(result);// it will stop here only due to runtime error it can't move in next line for excution.
+
+		System.out.println("Hii Vinay Kumar");
+		
+		System.out.println("Addition is:"+c);
+		
+	}
+}
